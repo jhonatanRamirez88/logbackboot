54c5d3e3cf3de7f03582b2d42ff6c6ad50564063
punki03 <tonny_jho@hotmail.com>
2018-07-26 09:17:16 -0500

    Proyecto base para pruebas de logback

---
+++ src/main/java/com/logbacboot/logbackboot/LogbackbootApplication.java
@@ -0,0 +1,12 @@
+package com.logbacboot.logbackboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackbootApplication.class, args);
+	}
+}
